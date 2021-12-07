f13935c88b2f1a780d606c59734c07cf423c08df
Paez11 <91887982+Paez11@users.noreply.github.com>
2021-12-07 14:34:36 +0100

    Add files via upload

--- 1Trimestre/GestionaNotas/src/Ejecutable.java
+++ 1Trimestre/GestionaNotas/src/Ejecutable.java
@@ -6,65 +6,65 @@ public class Ejecutable {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Alumno a1 = new Alumno("victor" , 8.67);
+//		Alumno a1 = new Alumno("victor" , 8.67);
+//		System.out.println(a1);
 		
-		System.out.println(a1);
+		Grupo g1 = new Grupo ("PrimeroSMR", 4);
+		Grupo g2 = new Grupo ("SegundoSMR", 3);
 		
-		Grupo g1 = new Grupo ("PrimeroSMR", 2);
+		
+		Alumno a1 = new Alumno("Victor" , 8.67);
+		g1.newAlumno(a1);
+		
+		Alumno a2 = new Alumno("Maria", 6.45);
+		g1.newAlumno(a2);
+		
+		Alumno a3 = new Alumno("Antonio", 5.55);
+		g1.newAlumno(a3);
+		
+		Alumno a4 = new Alumno("Quique", 9.45);
+		g1.newAlumno(a4);
+
 		System.out.println(g1);
 		
 		System.out.println();
 		
-		Grupo g2 = new Grupo ("SegundoSMR", 7);
+		Alumno a5 = new Alumno("Pepe" , 9.99);
+		g2.newAlumno(a5);
+		
+		Alumno a6 = new Alumno("Felipe", 3.25);
+		g2.newAlumno(a6);
+		
+		Alumno a7 = new Alumno("Lucia", 7.55);
+		g2.newAlumno(a7);
+		
 		System.out.println(g2);
 		
-		g1.newAlumno(a1);
-		
-		Alumno a2 = new Alumno("Sergio", 6.45);
-		g1.newAlumno(a2);
-		
-		System.out.println(g1);
-		
-		g1.mejorNota(g1);
-		
-		/*
-		g1.mejorNota(g1);
-		
-		/*
-		Alumno a2 = new Alumno("Boxhot", 7.45);
-		g1.newAlumno(a2);
-		
-		Alumno a4 = new Alumno("Pablo", 5.43);
-		g1.newAlumno(a4);
-		
-		System.out.println(g1);
+		System.out.println();
 		
 		g1.mediaGrupo(g1);
+		g2.mediaGrupo(g2);
 		
+		System.out.println();
 		
-		/*
-		g1.muestraAlumno("Boxhot");
+		g1.mejorNota(g1);
+		g1.peorNota(g1);
 		
+		System.out.println();
 		
-		g1.modificaNota("Boxhot");
+		g2.mejorNota(g2);
+		g2.peorNota(g2);
 		
-		System.out.println(g1);
+		System.out.println();
 		
+		g1.modificaNota("Victor");
+		System.out.println();
+		g2.modificaNota("Lucia");
 		
-		/*
-		g1.getAlumno(a2);
+		System.out.println();
 		
-		
-		Alumno a3 = new Alumno("Boxhot", 7.45);
-		g1.newAlumno(a3);
-		
-		System.out.println(g1);
-		
-		/*
-		g1.eliminaAlumno("Boxhot");
-		
-		System.out.println(g1);
-		*/
+		g1.muestraAlumno("Victor");
+		g1.eliminaAlumno("Victor");
 	}
 
 }
