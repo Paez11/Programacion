ab4e774dad2a9d3d5e4ffde6944e701f97f9534e
Paez11 <91887982+Paez11@users.noreply.github.com>
2021-12-17 14:35:14 +0100

    Add files via upload

---
+++ 1Trimestre/HelloWorld/src/Cadenas.java
@@ -0,0 +1,69 @@
+
+public class Cadenas {
+
+	public static void main(String[] args) {
+		
+		/*
+		String cadena1="Hola ";					//Crear cadena
+		String cadena2= new String("Mundo");	//Crear objeto
+		System.out.println(cadena1+cadena2);
+		
+		String cad = "Mi perro \"Perico\" es de color blanco";
+		System.out.println(cad);
+		
+		String cadena="Esto es un ejemplo \nde String de \nvarias lineas";
+		System.out.println(cadena);
+		
+		
+		String cadena1="Hola ";					
+		String cadena2="Mundo";
+		
+		if (cadena1==cadena2) {
+			System.out.println("Son iguales");
+		}else {
+			System.out.println("No son iguales");
+		}
+		
+		
+		String cadena1="Hola ";					
+		String cadena2="Mundo";
+		String cadena3="Ahora";
+		String cadena4="Hola ";
+		if (cadena1.compareTo(cadena2) == 0) {
+			System.out.println("Cadena1 y cadena2 son iguales");
+		}
+		else if (cadena3.compareTo(cadena2) < 0) {
+			System.out.println("cadena1 va antes que cadena2");
+		}
+		else if (cadena3.compareTo(cadena2) > 0) {
+			System.out.println("cadena1 va despues que cadena2");
+		}
+		System.out.println(cadena2);
+		System.out.println(cadena3);
+		System.out.println(cadena4);
+		
+		
+		String nombre= "Pedro";
+		String apellido1= "Ruiz";
+		String apellido2= "Aranda";
+		String nombreCompleto= nombre+" "+apellido1+" "+apellido2;
+		System.out.println(nombreCompleto);
+		
+		
+		String a="Resultado: "+3;
+		String b="Resultado: "+true;
+		String c="Resultado: "+ 'a';
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		*/
+		
+		var cadena1 = "HELLO WORLD";
+		var cadena2 = cadena1.charAt(5);
+		System.out.println(cadena2);
+		
+		String cad1 = "___Mi_perro_se_llama_Perico_____";
+		String cad2 = cad1.strip(); //cad2 vale "Mi perro se llama Perico"		
+	}
+
+}
